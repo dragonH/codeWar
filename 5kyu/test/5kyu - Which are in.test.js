579f7f1e06f13889650a5174f8a5fca3635ef5cf
DragonH <a0261038@g2.usc.edu.tw>
2019-02-26 21:18:46 +0800

    5kyu - Which are in.test.js

---
+++ 5kyu/test/5kyu - Which are in.test.js
@@ -0,0 +1,31 @@
+const InArray = require('../5kyu - Which are in');
+const should = require('should');
+
+const a2 = ["lively", "alive", "harp", "sharp", "armstrong"];
+describe('5kyu - Which are in', () => {
+    it('should return ["arp", "live", "strong"] for InArray(a1, a2)', (done) => {
+      a1 = ["arp", "live", "strong"];
+      InArray(a1, a2).should.eql(["arp", "live", "strong"]);
+      done();
+    });
+    it('should return ["live", "strong"] for InArray(a1, a2)', (done) => {
+      a1 = ["xyz", "live", "strong"];
+      InArray(a1, a2).should.eql(["live", "strong"]);
+      done();
+    });
+    it('should return ["arp", "live", "strong"] for InArray(a1, a2)', (done) => {
+      a1 = ["live", "strong", "arp"];
+      InArray(a1, a2).should.eql(["arp", "live", "strong"]);
+      done();
+    });
+    it('should return ["arp", "live", "strong"] for InArray(a1, a2)', (done) => {
+      a1 = ["live", "strong", "lyal", "lysh", "arp"]
+      InArray(a1, a2).should.eql(["arp", "live", "strong"]);
+      done();
+    });
+    it('should return [] for InArray(a1, a2)', (done) => {
+      a1 = []
+      InArray(a1, a2).should.eql([]);
+      done();
+    });            
+});

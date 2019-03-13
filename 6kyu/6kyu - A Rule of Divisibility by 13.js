8ca3e112015ef768b99b9fa1675f51b9cfeb8296
DragonH <a0261038@g2.usc.edu.tw>
2019-03-13 23:56:22 +0800

    add 6kyu - A Rule of Divisibility by 13

---
+++ 6kyu/6kyu - A Rule of Divisibility by 13.js
@@ -0,0 +1,18 @@
+function thirt(n) {
+  let pointer = [1,10,9,12,3,4];
+  while(String(n).length > 2) {
+    let arr = String(n).split('').reverse();
+    n = arr
+      .map((num, index) => {
+        let needle = 0;
+        if (index < 6) {
+          needle = index;
+        } else {
+          needle = index % 6;
+        }
+        return num * pointer[needle];
+        })
+        .reduce((pre, now, index) => pre + now);
+  }
+  return Number(n);
+}

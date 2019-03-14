762606e1e3cab3e504c3543f36b114601bd8fdff
DragonH <a0261038@g2.usc.edu.tw>
2019-03-15 00:22:53 +0800

    add 6kyu - Find The Parity Outlier.js

---
+++ 6kyu/6kyu - Find The Parity Outlier.js
@@ -0,0 +1,5 @@
+function findOutlier(integers){
+  const odd = integers.filter(integer => integer % 2 !== 0);
+  const even = integers.filter(integer => integer % 2 === 0);
+  return odd.length === 1 ? odd[0] : even[0];
+}

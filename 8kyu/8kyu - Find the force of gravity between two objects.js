dba9efb919182fd7105a5e7f3dba3b5b8257f2ad
DragonH <a0261038@g2.usc.edu.tw>
2019-04-03 23:17:05 +0800

    add 8kyu - Find the force of gravity between two objects.test.js

--- 8kyu/8kyu - Find the force of gravity between two objects.js
+++ 8kyu/8kyu - Find the force of gravity between two objects.js
@@ -1,4 +1,4 @@
-solution = ([obj1, obj2, dis], [u1, u2, u3]) => {
+module.exports = solution = ([obj1, obj2, dis], [u1, u2, u3]) => {
   const unitFormat = {
     kg: 1, g: 1e-3, mg: 1e-6, μg: 1e-9, lb: .453592,
      m: 1, cm: 1e-2, mm: 1e-3, μm: 1e-6, ft: .3048,

153aafdeddb33b66ed67b663d278fc2a1fc14d88
DragonH <a0261038@g2.usc.edu.tw>
2019-08-18 00:12:48 +0800

    Add 7kyu - Nth Smallest Element (Array Series-4).js & 7kyu - Nth Smallest Element (Array Series-4).test.js

---
+++ 7kyu/7kyu - Nth Smallest Element (Array Series-4)/7kyu - Nth Smallest Element (Array Series-4).js
@@ -0,0 +1,3 @@
+module.exports = function nthSmallest(arr, pos){
+  return arr.sort((a, b) => a - b)[pos - 1];
+};
---
+++ 7kyu/7kyu - Nth Smallest Element (Array Series-4)/test/7kyu - Nth Smallest Element (Array Series-4).test.js
@@ -0,0 +1,25 @@
+const nthSmallest = require('../7kyu - Nth Smallest Element (Array Series-4)');
+const should = require('should');
+
+describe('7kyu - Nth Smallest Element (Array Series-4)', () => {
+  it('Should reutrn 2 for nthSmallest([3,1,2],2)', (done) => {
+    should(nthSmallest([3,1,2],2)).equal(2);
+    done();
+  });
+  it('Should reutrn 7 for nthSmallest([15,20,7,10,4,3],3)', (done) => {
+    should(nthSmallest([15,20,7,10,4,3],3)).equal(7);
+    done();
+  });
+  it('Should reutrn -1 for nthSmallest([-5,-1,-6,-18],4)', (done) => {
+    should(nthSmallest([-5,-1,-6,-18],4)).equal(-1);
+    done();
+  });
+  it('Should reutrn -2 for nthSmallest([-102,-16,-1,-2,-367,-9],5)', (done) => {
+    should(nthSmallest([-102,-16,-1,-2,-367,-9],5)).equal(-2);
+    done();
+  });
+  it('Should reutrn 2 for nthSmallest(([2,169,13,-5,0,-1],4))', (done) => {
+    should(nthSmallest([2,169,13,-5,0,-1],4)).equal(2);
+    done();
+  });
+});
